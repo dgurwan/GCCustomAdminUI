31cf57be06a11de4998bcfb65a3ab37d99579be5
dgurwan <dgurwan@hotmail.com>
2023-04-14 08:47:00 +0200

    minor updates

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/genesys-cloud-sample'),
+      dir: require('path').join(__dirname, './coverage/CustomAdminUI'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'genesys-cloud-sample'`, () => {
+  it(`should have as title 'CustomAdminUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('genesys-cloud-sample');
+    expect(app.title).toEqual('CustomAdminUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('genesys-cloud-sample app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('CustomAdminUI app is running!');
   });
 });
